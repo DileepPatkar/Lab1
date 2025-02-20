315ec1a5d8aaad93ce6bd4818ab6c3a5af542926
Dileep <158464152+DileepPatkar@users.noreply.github.com>
2025-02-20 10:32:57 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [text, setText] = useState('')
 
   return (
-    <div className='flex items-center gap-10 w-full justify-center flex-col'>
-      <input className='px-8 py-6 rounded-lg w-[800px]' type="text" placeholder='Enter the text' value={text} onChange={(e) => {
-        setText(e.target.value)
-      }}></input>
-      <div className='flex gap-5 text-4xl '>
-        My Typed Text :
-        <div className='text-blue-400'>
-          {text}
-        </div>
+    
+      <div className="App">
+        <input
+        type="text"
+        placeholder="Type Something"
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+        />
+        <h1><strong> You Typed:{text}</strong></h1>
+        <h1><strong> number of characters: {text.length}</strong></h1>
       </div>
-      <div className='flex gap-5 text-4xl '>
-        Number of Characters:
-        <div className='text-blue-400'>
-          {text.length}
-        </div>
-      </div>
-    </div>
-  )
+      
+  );
 }
 
 export default App
